2fb4c9ca34c491bd3b075b61715cceddb373790c
GitWor <estevao.gomes@hotmail.com>
2022-05-25 18:01:57 -0300

    Changed Header Styles

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,10 +1,10 @@
 export function Header() {
   return (
     <div className="flex align-items-center justify-center">
-      <button className="flex inline-flex m-2 border-2" type="button">
+      <button className="m-2 p-2 border-2 rounded-full" type="button">
         Tasks
       </button>
-      <button className="flex inline-flex m-2 border-2" type="button">
+      <button className="m-2 p-2 border-2 rounded-full" type="button">
         New
       </button>
     </div>
