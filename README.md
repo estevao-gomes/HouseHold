0edca617365a2761d54dd59a1b39af8fce70c02b
GitWor <estevao.gomes@hotmail.com>
2022-05-25 17:55:38 -0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# vitejs-vite-yhgqs7
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/vitejs-vite-yhgqs7)
---
+++ src/components/Header.tsx
@@ -0,0 +1,12 @@
+export function Header() {
+  return (
+    <div className="flex align-items-center justify-center">
+      <button className="flex inline-flex m-2 border-2" type="button">
+        Tasks
+      </button>
+      <button className="flex inline-flex m-2 border-2" type="button">
+        New
+      </button>
+    </div>
+  );
+}
