784b29d9403c68ca57e6ca127331168c5bfcb837
GitWor <estevao.gomes@hotmail.com>
2022-05-31 16:37:35 -0300

    Added logic for date selection.

--- src/components/DateButton.tsx
+++ src/components/DateButton.tsx
@@ -4,14 +4,16 @@ import { DateSelection } from './DateSelection';
 export function DateButton() {
   return (
     //    <div className="justify-center flex items-center">
-    <Popover className="flex justify-center items-center">
-      <Popover.Panel >
-        <DateSelection/>
+    // <div className="ml-8 mt-8 bg-blue-500 p-4 rounded-full text-white font-bold">
+    // Date
+    // </div>
+    // <div className="flex justify-center justify-items-center items-center">
+    <Popover className="flex justify-center justify-items-center items-center">
+      <Popover.Panel className="mt-4">
+        <DateSelection />
       </Popover.Panel>
-      <Popover.Button>
-        <div className="ml-8 mt-8 bg-blue-500 p-4 rounded-full text-white font-bold">
-          Date
-        </div>
+      <Popover.Button className="mt-4 bg-blue-500 p-4 rounded-full text-white font-bold">
+        Date
       </Popover.Button>
     </Popover>
   );
--- src/components/DateSelection.tsx
+++ src/components/DateSelection.tsx
@@ -3,12 +3,35 @@ import { useState } from 'react';
 
 export function DateSelection() {
   const [date, setDate] = useState(new Date());
+  const [daysInMonth, setDaysInMonth] = useState(
+    new Date(date.getFullYear(), date.getMonth() + 1, 0).getDate()
+  );
+
+  const currentYear = new Date().getFullYear();
 
   function handleDaySet(day: number) {
     setDate((date) => {
-      return new Date(day, date.getMonth(), date.getFullYear());
-    });
+      return new Date(date.getFullYear(), date.getMonth(), day);
+    })
+    console.log(date) 
   }
+
+  function handleMonthSet(month: number) {
+    var newDaysInMonth = new Date(date.getFullYear(), month + 1, 0).getDate();
+    setDate((date) => {
+      return new Date(date.getFullYear(), month, date.getDate() > newDaysInMonth ? newDaysInMonth : date.getDate());
+    });
+    setDaysInMonth(newDaysInMonth)
+    console.log(date)
+  }
+
+  function handleYearSet(year: number) {
+    setDate((date) => {
+      return new Date(year, date.getMonth(), date.getDate());
+    })
+    console.log(date) 
+  }
+
   return (
     <div className=" mx-8 grid grid-cols-3 gap-x-4 bg-neutral-50 p-2">
       <div className="text-center">Dia</div>
@@ -18,18 +41,44 @@ export function DateSelection() {
         <div className="relative text-center">
           <Listbox.Button>{date.getDate()}</Listbox.Button>
           <Listbox.Options className="absolute max-h-[100px] overflow-auto">
-            {[...Array(31).keys()].map((day) => {
+            {[...Array(daysInMonth).keys()].map((day) => {
               return (
-                <Listbox.Option key={day} value={day}>
-                  {day}
+                <Listbox.Option key={day} value={day+1}>
+                  {day+1}
                 </Listbox.Option>
               );
             })}
           </Listbox.Options>
         </div>
       </Listbox>
-      <div>{date.getMonth()}</div>
-      <div>{date.getFullYear()}</div>
+      <Listbox value={date.getMonth()} onChange={handleMonthSet}>
+        <div className="relative text-center">
+          <Listbox.Button>{date.getMonth()+1}</Listbox.Button>
+          <Listbox.Options className="absolute max-h-[100px] overflow-auto">
+            {[...Array(12).keys()].map((month) => {
+              return (
+                <Listbox.Option key={month} value={month}>
+                  {month+1}
+                </Listbox.Option>
+              );
+            })}
+          </Listbox.Options>
+        </div>
+      </Listbox>
+      <Listbox value={date.getFullYear()} onChange={handleYearSet}>
+        <div className="relative text-center">
+          <Listbox.Button>{date.getFullYear()}</Listbox.Button>
+          <Listbox.Options className="absolute max-h-[100px] overflow-auto">
+            {[...Array(currentYear+1).keys()].filter((year) => year > 1989).map((year) => {
+              return (
+                <Listbox.Option key={year} value={year}>
+                  {year}
+                </Listbox.Option>
+              );
+            })}
+          </Listbox.Options>
+        </div>
+      </Listbox>
     </div>
   );
 }
