bb0ca381de303e6a6f9d509186d81f9c734e2501
estevao-gomes <estevaog.gomes@gmail.com>
2022-07-11 15:30:04 -0300

    [style]Adds styles for notes.

--- src/components/Notes/Note.tsx
+++ src/components/Notes/Note.tsx
@@ -1,12 +1,16 @@
-interface NoteProps{
-    name: string,
-    description: string
+interface NoteProps {
+  name: string;
+  description: string;
 }
-export function Note({name, description}:NoteProps){
-    return(
-        <div className="w-full border-2">
-            <div className="">{name}</div>
-            <div className="">{description}</div>
-        </div>
-    )
+export function Note({ name, description }: NoteProps) {
+  return (
+    <div className="border-2">
+      <div className="z-20 border-primary-dark bg-primary-light rounded-md w-fit px-1 -mb-1">
+        {name}
+      </div>
+      <div className="-mt-1 w-full border-2 border-primary-light bg-surface rounded-md">
+        {description}
+      </div>
+    </div>
+  );
 }
