3772166bed6b9d934e55457d2be57ca5edb77837
estevao-gomes <estevaog.gomes@gmail.com>
2022-07-11 17:57:13 -0300

    [style]Adds scrollbar to notes.

--- src/App.tsx
+++ src/App.tsx
@@ -5,7 +5,7 @@ export function App() {
   return (
     <DateContextProvider>
       <div className='md:grid md:grid-cols-3'>
-        <TaskListing style="mx-auto mt-4 min-h-[30rem] max-h-[45rem]" />
+        <TaskListing style="mx-auto mt-4 min-h-[30rem] max-h-full overflow-y-scroll scrollbar-thin scrollbar-thumb-primary-dark scrollbar-track-surface " />
         <Notes style="border-4 border-primary"/>
         <ShoppingList style="border-4 border-primary"/>
         <BillsPayment style="md:col-span-full border-4 border-primary"/>
--- src/components/Notes/Note.tsx
+++ src/components/Notes/Note.tsx
@@ -8,7 +8,7 @@ export function Note({ name, description }: NoteProps) {
       <div className="z-20 border-primary-dark bg-primary-light rounded-md w-fit px-2 mb-1">
         {name}
       </div>
-      <div className="w-full border-2 border-primary-light bg-surface rounded-md max-h-[12rem] overflow-y-scroll">
+      <div className="w-full border-2 p-2 border-primary-light bg-surface rounded-md max-h-[12rem] overflow-y-scroll scrollbar-thin scrollbar-thumb-primary-dark scrollbar-track-surface">
         {description}
       </div>
     </div>
